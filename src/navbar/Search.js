300fdfb119c9a4e8f3c74bf984a814c15314ebb5
Daniel <daniel.corbellini28@gmail.com>
2025-01-17 19:31:48 -0300

    167. Using Refs to select DOM elements

--- src/navbar/Search.js
+++ src/navbar/Search.js
@@ -1,4 +1,25 @@
+import { useRef, useEffect } from "react";
+
 export function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputEl.current) return;
+
+        if (e.code === "Enter") {
+          inputEl.current.focus();
+          setQuery("");
+        }
+      }
+      document.addEventListener("keydown", callback);
+      inputEl.current.focus();
+      return () => document.removeEventListener("keydown", callback);
+    },
+    [setQuery]
+  );
+
   return (
     <input
       className="search"
@@ -6,6 +27,7 @@ export function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
